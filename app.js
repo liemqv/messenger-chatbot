31c85606cad1a3a9348e9e682e9e00525864cc6c
Quang Liem <liemqv@gmail.com>
2017-11-06 17:30:30 +0700

    Update app.js

--- app.js
+++ app.js
@@ -83,6 +83,8 @@ app.get('/webhook', function(req, res) {
 app.post('/webhook', function (req, res) {
   var data = req.body;
 
+  console.log('liemqv - body = ' + data);
+  
   // Make sure this is a page subscription
   if (data.object == 'page') {
     // Iterate over each entry
