fdecbca0268dae5e2a93dc3ffb5a6955253c6368
Quang Liem <liemqv@gmail.com>
2017-11-09 16:13:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -864,16 +864,7 @@ app.get('/facebook', function (req, res) {
 	res.sendFile('public/chat.html');
 });
 
-
-//END: ============================================================ Socket.IO
-
-// Start server
-// Webhooks must be available via SSL with a certificate signed by a valid 
-// certificate authority.
-server.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
+/*
 io.configure('production', function(){
   io.enable('browser client etag');
   io.set('log level', 1);
@@ -890,6 +881,7 @@ io.configure('production', function(){
 io.configure('development', function(){
   io.set('transports', ['websocket']);
 });
+*/
 
 io.sockets.on('connection', function(client){
     var userName;
@@ -918,6 +910,15 @@ io.sockets.on('connection', function(client){
 			console.log("anonymous left");
 		}
     });
+});
+
+//END: ============================================================ Socket.IO
+
+// Start server
+// Webhooks must be available via SSL with a certificate signed by a valid 
+// certificate authority.
+server.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
 module.exports = app;
