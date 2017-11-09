bcd56c6dd9c744ddf127cc2a64946d573b441523
Quang Liem <liemqv@gmail.com>
2017-11-09 17:18:52 +0700

    Update app.js

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
-//app.use(express.static('public'));
-app.use('/public', express.static('public'));
+app.use(express.static('public'));
+//app.use('/public', express.static('public'));
 
 //Array luu danh sach request
 var received_updates = [];
